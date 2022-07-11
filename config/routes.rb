49b5b243267ecccd8fda0f014f4169ca764950c3
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-11 04:35:14 +0000

    [Add]サインイン・ログアウト・サインアップ・About画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get 'homes/about', to: 'homes#show', as: :about
+  ##about_pathとabout_urlが名前付きルーティングヘルパーとして作成される
+  ##about_pathを呼び出すと/homes/aboutが返される
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
