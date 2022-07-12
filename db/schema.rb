348eb590a7dab453adc9f7a4143fd92429b87b92
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-12 14:32:15 +0000

    [Add]投稿一覧ページ作成・遷移設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_105147) do
+ActiveRecord::Schema.define(version: 2022_07_12_121246) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 2022_07_11_105147) do
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
   end
 
   create_table "postimages", force: :cascade do |t|
