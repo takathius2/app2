ba9d9c05a237652bb3ce634fc2cff752a01d3f51
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-16 15:08:21 +0000

    [Add]ユーザーの編集・保存機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+  get "homes/about", to: "homes#show", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
-  get 'homes/about', to: 'homes#show', as: :about
+  resources :users, only: [:show, :edit, :update]
   ##about_pathとabout_urlが名前付きルーティングヘルパーとして作成される
   ##about_pathを呼び出すと/homes/aboutが返される
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
